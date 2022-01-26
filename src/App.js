7c7876b88625914eb136bb672263122a8c47fff6
MestreALMO <lusegardis@gmail.com>
2022-01-26 20:52:22 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTheme, useMediaQuery, Box, Paper } from "@mui/material";
+
+const BreakpointHelper = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
+  if (matches) {
+    return (
+      <Paper elevation={5}>
+        <Box p={5}>
+          BreakpointHelper will render everything up from 'sm' which is 600px
+        </Box>
+      </Paper>
+    );
+  }
+  return <></>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box>
+        <h1>Test</h1>
+        <BreakpointHelper />
+      </Box>
+    </>
   );
 }
 
